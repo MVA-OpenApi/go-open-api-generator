54ecce164346a3c5b168b426621a0dd5be6c25df
Adrian Munteanu <adrian.munteanu@hybridheroes.de>
2022-05-15 13:21:08 +0300

    feat: add generation of basic folder structure

---
+++ generator/fileUtils.go
@@ -0,0 +1,31 @@
+package generator
+
+import (
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func generateFolder(path string) {
+	err := os.MkdirAll(path, os.ModePerm)
+
+	check(err)
+}
+
+func deleteFolderRecursively(path string) {
+	err := os.RemoveAll(path)
+
+	check(err)
+}
+
+func generateFile(path string) {
+	file, err := os.Create(path)
+
+	check(err)
+
+	defer file.Close()
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	gen "go-open-api-generator/generator"
+)
+
+func main() {
+	gen.CreateBuildDirectory()
+}
