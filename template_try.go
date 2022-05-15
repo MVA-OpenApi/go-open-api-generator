04f859e0b934fb373fe700a34e4a183c1884b640
fjoniyzeiri <fjoniyzeiri@campus.tu-berlin.de>
2022-05-15 19:47:38 +0200

    added template_try

---
+++ template_try.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+        "fmt"   
+        "os"
+        "text/template"
+        "encoding/json"
+        "io/ioutil"
+        "bytes"
+)
+
+// func createFile(name string) (fp *os.File){
+//     nameOfFile := parseFileName(name)
+//     nameOfFile = nameOfFile + ".go"
+//     f, err := os.Create(nameOfFile)
+//     check(err)
+//     defer f.Close()
+//     return f
+// }
+
+func check(e error){
+    if e != nil {
+        panic(e)    
+    }
+}
+
+func parseFileName(filename string) string {
+    s := filename[:len(filename)-5]
+    return s
+}
+
+func main() {
+    file, _ := ioutil.ReadFile("example.json")  
+    m := map[string]interface{}{}
+    err := json.Unmarshal(file, &m)
+    if err != nil {
+        fmt.Println(err.Error)
+    }
+    filename := parseFileName("example.json")
+    fileToWrite, err := os.Create(filename+".go")
+    // filename := parseFileName("example.json")
+    var restArray []string
+    var strArray []string
+    for _, record := range m {
+        if rec, ok := record.(map[string]interface{}); ok {
+            for key, val := range rec {
+                val1 := fmt.Sprint(val)
+                restArray = append(restArray, key)
+                strArray = append(strArray, val1)
+            }
+        } 
+    }
+    for i := 0; i < len(restArray); i++{
+        temp, err := template.ParseFiles("template_example.tmpl")
+        if err != nil {
+            fmt.Println(err)
+        }
+        var tpl bytes.Buffer
+        input := temp.Execute(&tpl, strArray[i])
+        if(input != nil){
+            fmt.Println("Error")
+        }
+        result := tpl.String()
+            // f.WriteString(string(input))
+        fmt.Fprintf(fileToWrite, "%+v", result)
+    }
+    //We want to iterate inside type map[coordinate:map[maximum:4 minimum:1 type:integer]] struct {}
+}
+
+
+
+
+
