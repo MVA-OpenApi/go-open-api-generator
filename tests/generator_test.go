f3268681d2f7a4682bbec630ce2ff4a73b1bddf0
fjoniyzeiri <fjoniyzeiri@campus.tu-berlin.de>
2022-05-29 19:00:28 +0200

    test handlers and yaml with false name

--- tests/generator_test.go
+++ tests/generator_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	fs "go-open-api-generator/fileUtils"
 	generator "go-open-api-generator/generator"
 	"os"
 	"testing"
@@ -17,11 +18,26 @@ func TestGenerationOfProject(t *testing.T) {
 }
 
 func TestGenerationOfProjectWithFalseName(t *testing.T) {
+	fs.DeleteFolderRecursively("./build")
 	config := generator.GeneratorConfig{OpenAPIPath: "stres.yaml", OutputPath: "./build", ModuleName: "build"}
 	generator.GenerateServer(config)
-	if _, err := os.Stat("./build"); err != nil {
-		if !os.IsNotExist(err) {
-			t.Errorf("Project generated but yaml file does not exist")
+	_, err := os.Stat("./build")
+	if !os.IsNotExist(err) {
+		t.Errorf("Project generated but yaml file does not exist")
+	}
+}
+
+func TestHandlers(t *testing.T) {
+	fs.DeleteFolderRecursively("./build")
+	config := generator.GeneratorConfig{OpenAPIPath: "stores.yaml", OutputPath: "./build", ModuleName: "build"}
+	generator.GenerateServer(config)
+	handlers := []string{"createStore.go", "deleteStoreByID.go", "getAllStores.go", "getStoreByID.go", "updateStoreByID.go",
+		"handler.go"}
+	for _, name := range handlers {
+		if _, err := os.Stat("./build/pkg/handler/" + name); err != nil {
+			if os.IsNotExist(err) {
+				t.Errorf("Handler " + name + " not generated")
+			}
 		}
 	}
 }
