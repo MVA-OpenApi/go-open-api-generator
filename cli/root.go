5f8ca8dd5f4073c0f24c4b54e9114bc3fd815d92
Adrian Munteanu <adrian.munteanu@hybridheroes.de>
2022-06-08 16:33:24 +0200

    feat: add database flag to cli

--- cli/root.go
+++ cli/root.go
@@ -12,22 +12,23 @@ import (
 
 // variables for the flags
 var (
-	projectPath string
-	projectName string
-	loggerFlag  bool
+	projectPath  string
+	projectName  string
+	loggerFlag   bool
+	databaseFlag bool
 )
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "go-open-api-generator",
 	Short: "Create server and client API code from OpenApi Spec",
-	Long:  "Generate Go-Server code and ReactJS-Clientcode for your application by providing an OpenAPI Specification",
+	Long:  "Generate Go-Server code and RapidDoc-Clientcode for your application by providing an OpenAPI Specification",
 }
 
 var generateCmd = &cobra.Command{
 	Use:     "generate <path to OpenAPI Specification>",
 	Short:   "Create server and client API code from OpenApi Spec",
-	Long:    "Generate Go-Server code and ReactJS-Clientcode for your application by providing an OpenAPI Specification",
+	Long:    "Generate Go-Server code and RapidDoc-Clientcode for your application by providing an OpenAPI Specification",
 	Example: "generate ./stores.yaml -o ./outputPath -n StoresAPI",
 	Args:    cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
@@ -50,7 +51,7 @@ var generateCmd = &cobra.Command{
 			ModuleName:   projectName,
 			DatabaseName: "database",
 			Flags: gen.Flags{
-				UseDatabase: true,
+				UseDatabase: databaseFlag,
 				UseLogger:   loggerFlag,
 			},
 		}
@@ -83,6 +84,7 @@ func init() {
 	generateCmd.Flags().StringVarP(&projectPath, "output", "o", "", "path where generated code gets stored (default is the home directory)")
 	generateCmd.Flags().StringVarP(&projectName, "name", "n", "", "module name of generated code (default is 'build')")
 	generateCmd.Flags().BoolVarP(&loggerFlag, "logger", "l", false, "use logging middleware in generated code (default is 'false')")
+	generateCmd.Flags().BoolVarP(&databaseFlag, "database", "d", false, "add sqlite3 database in generated code (default is 'false')")
 
 	// add generate command
 	rootCmd.AddCommand(generateCmd)
